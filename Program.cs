c54dd2b32cb3d9a2250755f12cd759242550d464
ametsforal <aforalgarc1@educacion.navarra.es>
2025-11-15 22:30:49 +0100

    feat:  update Proiektua.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Proiektua ll = new();
+        ll.Run();
+    }
+}
